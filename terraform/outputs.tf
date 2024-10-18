eaaf2333c2f985a5c385980f32fa7cc0b5172ce6
Bode F <bode.falade@outlook.com>
2024-10-18 08:04:52 -0500

    update output to reflect acm import.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,8 @@
 # Output for the ACM Certificate ARN
+# Output for the ACM Certificate ARN
 output "jv_magic_certificate_arn" {
-  description = "The ARN of the validated ACM Certificate for jv-magic.com"
-  value       = aws_acm_certificate_validation.jv_magic_cert_validated.certificate_arn
+  description = "The ARN of the ACM Certificate for jv-magic.com"
+  value       = data.aws_acm_certificate.jv_magic_cert.arn
 }
 
 # Output for the Route53 Hosted Zone ID
