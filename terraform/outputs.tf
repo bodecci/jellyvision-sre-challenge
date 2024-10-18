c997a4582e4310ef7d66d10701169bf93d871894
Bode F <bode.falade@outlook.com>
2024-10-17 21:56:40 -0500

    updated outputs file.

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+# Output for the ACM Certificate ARN
+output "jv_magic_certificate_arn" {
+  description = "The ARN of the validated ACM Certificate for jv-magic.com"
+  value       = aws_acm_certificate_validation.jv_magic_cert_validated.certificate_arn
+}
+
+# Output for the Route53 Hosted Zone ID
+output "jv_magic_zone_id" {
+  description = "The Route53 Hosted Zone ID for jv-magic.com"
+  value       = aws_route53_zone.jv_magic_zone.zone_id
+}
