1d182ac0a47e914bad3ab0997e10e77a7ae43b17
Bode F <bode.falade@outlook.com>
2024-10-18 00:08:02 -0500

    added IAM Role.

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,6 +167,30 @@ resource "aws_lb_listener" "https_listener" {
   }
 }
 
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the necessary ECS Task Execution Role policy
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 
 # ECS Cluster
 resource "aws_ecs_cluster" "simpsons_cluster" {
@@ -180,6 +204,9 @@ resource "aws_ecs_task_definition" "simpsons_task" {
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+
+  
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -205,6 +232,7 @@ resource "aws_ecs_task_definition" "simpsons_task" {
     }
   ])
 }
+
 
 # ECS Service
 resource "aws_ecs_service" "simpsons_service" {
